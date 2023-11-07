c8a3b4d7e2cc14225ab05594441be6940a5a5eba
lskarada <69657694+lskarada@users.noreply.github.com>
2023-11-07 10:41:24 -0800

    Add files via upload

---
+++ api/main-tf-serving.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../saved_models/1")
+# endpoint = "http://localhost:8000/v1/models/potatoes_model:predict"
+
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+# http://localhost:8000/ping
+@app.get("/ping")
+async def ping():
+    return "I'm alive"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = Image.open(BytesIO(data))
+    # Convert the Image object to an ndarray object
+    image_array = np.array(image)
+    return image_array
+
+
+# inputing a file i.e a picture
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        "class": predicted_class,
+        "confidence": confidence
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
